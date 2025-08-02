c1dab2ec7aa0314e884e0e495cc89999126b963c
asseternity <nakupovasset@gmail.com>
2025-08-02 20:15:40 +0400

    fix: update sql file names

--- sql/07_law_grads_work_fields.sql
+++ sql/07_law_grads_work_fields.sql
@@ -1,131 +1,148 @@
 SELECT
-    exp.person_id,
-    exp.title,
+    edu.person_id,
     edu.program,
+    exp.title,
     CASE
-        WHEN edu.program LIKE "%law%"
-        OR edu.program LIKE "%legal%"
-        OR edu.program LIKE "%juris%"
-        OR edu.program LIKE "%llb"
-        OR edu.program LIKE '%crimin%'
-        OR edu.program LIKE '%justic%'
-        OR edu.program LIKE "%llb%"
-        OR edu.program LIKE "llb"
-        OR edu.program LIKE "llm"
-        OR edu.program LIKE "llm%"
-        OR edu.program LIKE "%llm"
-        OR edu.program LIKE "%jd"
-        OR edu.program LIKE "jd"
-        OR edu.program LIKE "%jd%" THEN "Law"
-        WHEN edu.program LIKE "%busines%"
-        OR edu.program LIKE '%administrat%'
-        OR edu.program LIKE "%economic%"
-        OR edu.program LIKE '%account%'
-        OR edu.program LIKE '%finan%'
-        OR edu.program LIKE '%market%'
-        OR edu.program LIKE '%commerce%'
-        OR edu.program LIKE '%trade%'
-        OR edu.program LIKE '%leadershi%'
-        OR edu.program LIKE '%manage%'
-        OR edu.program LIKE "%audi%" THEN "Business, Accounting and Audit"
-        WHEN edu.program LIKE '%compute%'
-        OR edu.program LIKE '%softwar%'
-        OR edu.program LIKE '%cyber%'
-        OR edu.program LIKE '%computing%'
-        OR edu.program LIKE '%programmin%'
-        OR edu.program LIKE '%backend%'
-        OR edu.program LIKE '%back end%'
-        OR edu.program LIKE '%back-end%'
-        OR edu.program LIKE '%frontend%'
-        OR edu.program LIKE '%front end%'
-        OR edu.program LIKE '%front-end%'
-        OR edu.program LIKE '%developer%'
-        OR edu.program LIKE '%information system%'
-        OR edu.program LIKE '%information technolog%' THEN 'Computer Science and IT'
-        WHEN edu.program LIKE '%data%' THEN 'Data and Databases'
-        WHEN edu.program LIKE '%math%'
-        OR edu.program LIKE '%physics%' THEN 'Math and Physics'
-        WHEN edu.program LIKE '%religio%'
-        OR edu.program LIKE '%theol%'
-        OR edu.program LIKE '%psycholog%'
-        OR edu.program LIKE '%cognitive%'
-        OR edu.program LIKE '%philosoph%' THEN 'Psychology and Religion'
-        WHEN edu.program LIKE '%health%'
-        OR edu.program LIKE '%medic%'
-        OR edu.program LIKE '%dentist%'
-        OR edu.program LIKE '%nurs%'
-        OR edu.program LIKE '%plant%'
-        OR edu.program LIKE '%animal%'
-        OR edu.program LIKE '%agricult%'
-        OR edu.program LIKE '%chemistr%'
-        OR edu.program LIKE '%biochem%'
-        OR edu.program LIKE '%pharm%'
-        OR edu.program LIKE '%bio%' THEN 'Chemistry, Biology, Bioengineering and Health'
-        WHEN edu.program LIKE '%scien%' THEN 'Science'
-        WHEN edu.program LIKE '%communicatio%' THEN 'Communications'
-        WHEN edu.program LIKE '%architectur%'
-        OR edu.program LIKE '%urban%'
-        OR edu.program LIKE '%landscape%'
-        OR edu.program LIKE '%interior%'
-        OR edu.program LIKE '%civil%'
-        OR edu.program LIKE '%constructio%'
-        OR edu.program LIKE '%buildin%'
-        OR edu.program LIKE '%mechani%'
-        OR edu.program LIKE '%electr%' THEN 'Electrical & Construction Engineering'
-        WHEN edu.program LIKE '%social%'
-        OR edu.program LIKE '%journalis%'
-        OR edu.program LIKE '%humanit%'
-        OR edu.program LIKE '%political%'
-        OR edu.program LIKE '%histor%'
-        OR edu.program LIKE '%literatur%' THEN 'Political Science, History, Journalism and Literature'
-        WHEN edu.program LIKE '%art%'
-        OR edu.program LIKE '%music%'
-        OR edu.program LIKE '%photograph%'
-        OR edu.program LIKE '%theat%'
-        OR edu.program LIKE '%desig%'
-        OR edu.program LIKE '%film%' THEN 'Art, design, music and film'
-        WHEN edu.program LIKE '%high schoo%'
-        OR edu.program LIKE '%course%'
-        OR edu.program LIKE '%certificat%' THEN 'High school, certificate or course'
-        WHEN edu.program LIKE '%spanis%'
-        OR edu.program LIKE '%englis%'
-        OR edu.program LIKE '%languag%' THEN 'Languages'
-        WHEN edu.program LIKE '%genera%' THEN 'General Studies'
-        WHEN edu.program LIKE '%engineerin%' 
-        OR edu.program LIKE '%aviatio%' THEN 'Other Engineering'
-        WHEN edu.program IS NULL
-        OR edu.program = 'unknown' THEN 'Unknown'
-        ELSE "Other"
-    END AS 'degree',
-    CASE
-        WHEN exp.title LIKE '%senior%' THEN "Senior"
-        WHEN  exp.title LIKE '%junior%' THEN "Junior"
-        ELSE "Other"
-    END AS 'level'
+    -- Legal (first to avoid misclassification as Software or Other)
+    WHEN exp.title LIKE '%lawye%'
+      OR exp.title LIKE '%attorne%'
+      OR exp.title LIKE '%legal%'
+      OR exp.title LIKE '%paralega%'
+      OR exp.title LIKE '%solicito%'
+      OR exp.title LIKE '%barriste%'
+      OR exp.title LIKE '%counsel%'
+      OR exp.title LIKE '%juris%'
+      OR exp.title LIKE '%llm%'
+      OR exp.title LIKE '%llb%'
+      OR exp.title LIKE '%jd%'
+      OR exp.title LIKE '%regulatory%'
+      OR exp.title LIKE '%compliance%'
+      OR exp.title LIKE '%ethics%'
+      OR exp.title LIKE '%policy advis%'
+      OR exp.title LIKE '%public policy%'
+      OR exp.title LIKE '%general counsel%' THEN 'Legal'
+
+    -- Business & Consulting
+    WHEN exp.title LIKE '%business analys%'
+      OR exp.title LIKE '%strategy%'
+      OR exp.title LIKE '%consultan%'
+      OR exp.title LIKE '%business development%' THEN 'Business & Consulting'
+
+    -- Management
+    WHEN exp.title LIKE '%manager%'
+      OR exp.title LIKE '%project lead%'
+      OR exp.title LIKE '%product manager%'
+      OR exp.title LIKE '%executive%'
+      OR exp.title LIKE '%director%'
+      OR exp.title LIKE '%vp of%'
+      OR exp.title LIKE '%supervisor%'
+      OR exp.title LIKE '%scrum master%'
+      OR exp.title LIKE '%head of%' THEN 'Management'
+
+    -- Human Resources
+    WHEN exp.title LIKE '%recruite%'
+      OR exp.title LIKE '%talent%'
+      OR exp.title LIKE '%human resources%'
+      OR exp.title LIKE '%hr%' THEN 'Human Resources'
+
+    -- Accounting & Finance
+    WHEN exp.title LIKE '%accountan%'
+      OR exp.title LIKE '%finance%'
+      OR exp.title LIKE '%financial%'
+      OR exp.title LIKE '%audito%'
+      OR exp.title LIKE '%controlle%' THEN 'Accounting & Finance'
+
+    -- Data Science and AI
+    WHEN exp.title LIKE '%data scientis%'
+      OR exp.title LIKE '%data science%'
+      OR exp.title LIKE '%data analys%'
+      OR exp.title LIKE '%data analysi%'
+      OR exp.title LIKE '%data enginee%'
+      OR exp.title LIKE '%machine learning%'
+      OR exp.title LIKE '%ml enginee%'
+      OR exp.title LIKE '%ai enginee%'
+      OR exp.title LIKE '%artificial intelli%' THEN 'Data Science & AI'
+
+    -- Software and IT
+    WHEN exp.title LIKE '%software%'
+      OR exp.title LIKE '%frontend%'
+      OR exp.title LIKE '%backend%'
+      OR exp.title LIKE '%fullstack%'
+      OR exp.title LIKE '%front-end%'
+      OR exp.title LIKE '%front end%'
+      OR exp.title LIKE '%back-end%'
+      OR exp.title LIKE '%back end%'
+      OR exp.title LIKE '%full-stack%'
+      OR exp.title LIKE '%programme%'
+      OR exp.title LIKE '%devops%'
+      OR exp.title LIKE '%qa enginee%'
+      OR exp.title LIKE '%it security%'
+      OR exp.title LIKE '%it specialis%'
+      OR exp.title LIKE '%cloud%'
+      OR exp.title LIKE '%cyber%'
+      OR exp.title LIKE '%systems analys%'
+      OR exp.title LIKE '%technical suppor%'
+      OR exp.title LIKE '%information security analys%'
+      OR exp.title LIKE '%it analys%'
+      OR exp.title LIKE '%network engine%'
+      OR exp.title LIKE '%web develope%'
+      OR exp.title LIKE '%it suppor%'
+      OR exp.title LIKE '%system admin%' THEN 'Software & IT'
+
+    -- Marketing & Communications
+    WHEN exp.title LIKE '%marketing%'
+      OR exp.title LIKE '%communications%'
+      OR exp.title LIKE '%social media%'
+      OR exp.title LIKE '%pr%'
+      OR exp.title LIKE '%brand%' THEN 'Marketing & Communications'
+
+    -- Sales
+    WHEN exp.title LIKE '%sales%'
+      OR exp.title LIKE '%account executive%'
+      OR exp.title LIKE '%business development%' THEN 'Sales'
+
+    -- Education & Volunteering
+    WHEN exp.title LIKE '%teache%'
+      OR exp.title LIKE '%educato%'
+      OR exp.title LIKE '%lecture%'
+      OR exp.title LIKE '%professo%'
+      OR exp.title LIKE '%voluntee%'
+      OR exp.title LIKE '%tuto%' THEN 'Education & Volunteering'
+
+    -- Administrative
+    WHEN exp.title LIKE '%admin%'
+      OR exp.title LIKE '%assistant%'
+      OR exp.title LIKE '%office manage%'
+      OR exp.title LIKE '%secretar%' THEN 'Administrative'
+
+    -- Design and UX/UI
+    WHEN exp.title LIKE '%ux%'
+      OR exp.title LIKE '%ui designe%'
+      OR exp.title LIKE '%product designe%'
+      OR exp.title LIKE '%graphic designe%'
+      OR exp.title LIKE '%visual designe%' THEN 'Design & UX/UI'
+
+    -- Armed Security
+    WHEN exp.title LIKE '%armed%'
+      OR exp.title LIKE '%enforcemen%'
+      OR exp.title LIKE '%security guard%' THEN 'Armed Security'
+
+    -- Unknown or not provided
+    WHEN exp.title IS NULL 
+      OR exp.title = 'unknown' THEN 'Unknown'
+
+    -- Everything else
+    ELSE 'Other'
+  END AS 'field'
 FROM
-    experience_table_clean AS exp
-    JOIN education_table_clean AS edu ON exp.person_id = edu.person_id
+    education_table_clean AS edu
+    JOIN experience_table_clean AS exp ON edu.person_id = exp.person_id
 WHERE
-    exp.title LIKE '%developer'
-    OR exp.title LIKE '%software%'
-    OR exp.title LIKE '%frontend$'
-    OR exp.title LIKE '%front end$'
-    OR exp.title LIKE '%backend%'
-    OR exp.title LIKE '%back end%'
-    OR exp.title LIKE '%fullstack%'
-    OR exp.title LIKE '%front-end%'
-    OR exp.title LIKE '%back-end%'
-    OR exp.title LIKE '%full-stack%'
-    OR exp.title LIKE '%programme%'
-    OR exp.title LIKE '%devops%'
-    OR exp.title LIKE '%data enginee%'
-    OR exp.title LIKE '%qa enginee%'
-    OR exp.title LIKE '%ux/ui%'
-    OR exp.title LIKE '%data scien%'
-    OR exp.title LIKE '%data analy%'
-    OR exp.title LIKE '%cloud%'
-    OR exp.title LIKE '%cyber%'
-    OR exp.title LIKE '%web develope%'
-    OR exp.title LIKE '%system admin%'
-    OR exp.title LIKE '%machine learn%'
-    OR exp.title LIKE '%artificial int%'
+    edu.program LIKE '%law%'
+    OR edu.program LIKE '%legal%'
+    OR edu.program LIKE '%juris%'
+    OR edu.program LIKE '%llb'
+    OR edu.program LIKE '%crimin%'
+    OR edu.program LIKE '%justic%'
+    OR edu.program LIKE '%llm'
+    OR edu.program LIKE '%jd'
